680e124bd1f8e359bb5feb3927413a180d440363
AhmadV90 <scriptgenerator0@gmail.com>
2025-07-22 20:12:48 +0800

    Update Speed%20Hub%20X.lua

--- Speed%20Hub%20X.lua
+++ Speed%20Hub%20X.lua
@@ -1,7 +1,2 @@
-task.spawn(function()
-    loadstring(game: HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
-end)
-
-task.spawn(function()
-    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/W9BFjT1TV9fIbfKx/raw", true))()
-end)
+  loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/W9BFjT1TV9fIbfKx/raw", true))()
+loadstring(game: HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
