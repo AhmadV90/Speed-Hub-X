7312ac5020e53a7f67c3a5bd2b0c0ab9ef721fbe
AhmadV90 <scriptgenerator0@gmail.com>
2025-07-27 14:20:09 +0800

    Update Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://pastefy.app/0b708Rqk/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/2mWZnLw6/raw"))()
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/GXmtiPRKmxi8j7lR/raw", true))()
 loadstring(game: HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
