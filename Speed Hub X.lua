c08466aefaeceec2f994467c10d35d9eab672507
AhmadV90 <scriptgenerator0@gmail.com>
2025-07-27 12:45:55 +0800

    Update and rename Speed%20Hub%20X.lua to Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,2 +1,2 @@
-  loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/W9BFjT1TV9fIbfKx/raw", true))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/GXmtiPRKmxi8j7lR/raw", true))()
 loadstring(game: HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
